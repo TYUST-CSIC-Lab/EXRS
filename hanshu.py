b9441a14c473b597f08709e49ba308d9447349be
TYUST-CSIC-Lab <97212884+TYUST-CSIC-Lab@users.noreply.github.com>
2022-01-10 20:13:17 +0800

    Add files via upload

---
+++ hanshu.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import random
+from random import choice
+import warnings
+warnings.filterwarnings("ignore")
+import numpy as np
+
+shuju1 = pd.read_csv(r'C:\Users\Administrator\Desktop\event.csv', engine='python', encoding="utf-8")
+houxuanfinal = shuju1.values
+
+shuju4 = pd.read_csv(r'C:\Users\Administrator\Desktop\sim.csv', engine='python', encoding="utf-8")
+sim = shuju4.values
+
+shuju5 = pd.read_csv(r'C:\Users\Administrator\Desktop\topsuoyin.csv', engine='python', encoding="utf-8")
+topsuoyin = shuju5.values
+
+
+def mubiaohanshu1(suoyin,D,topsuoyin):
+    common = [x for x in suoyin if x in topsuoyin]
+    f1=1-len(common)/D
+    return f1
+
+
+def mubiaohanshu2(suoyin, D,houxuanfinal):
+    bizhi=[]
+    for s in range(len(suoyin)):
+        itemsuoyin=int(suoyin[s])
+        rated=float(houxuanfinal[itemsuoyin][4])/93.5
+        bizhi.append(rated*houxuanfinal[itemsuoyin][9])
+    f2=1-sum(bizhi)
+    return f2
+
+
+
+def mubiaohanshu3(suoyin, D,houxuanfinal,sim):
+    itemid = list()
+    similarity = []
+    simtemp = np.zeros(shape=[1, 10000])
+    for b in range(len(suoyin)):
+        itemsuoyin = int(suoyin[b])
+        itemid.append(houxuanfinal[itemsuoyin][5])
+    newitemid = sorted(itemid)
+    for c in range(len(suoyin)):
+        e = c
+        for d in range(len(sim)):
+            if (newitemid[c] == sim[d][0]):
+                simtemp = sim[d]
+        while (e < len(newitemid) - 1):
+            e = e + 1
+            similarity.append(simtemp[newitemid[e]])
+    f3 = 2 * sum(similarity) / (D * (D - 1))
+    return f3
+
+
+def mubiaohanshu4(suoyin, D,houxuanfinal):
+    novelty = list()
+    for i in range(len(suoyin)):
+        itemsuoyin = int(suoyin[i])
+        novelty.append(houxuanfinal[itemsuoyin][11])
+    f4 = sum(novelty) / D
+    return f4
+
+
+
+
+
+
+
